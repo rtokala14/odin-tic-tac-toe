2b221b23c8e20bf8003e714fb7bb516cd5611fcc
Rohit Tokala <rtokala@purdue.edu>
2022-06-09 14:05:57 +0400

    Add color scheme and button animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ["*.html","./dist/**/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-dark": "#393E41",
+        "primary-light": "#D3D0CB",
+      },
+    },
   },
   plugins: [],
 }
